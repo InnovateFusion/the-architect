388ef4104963d92322d3429ee49f52884d0c1372
Bisrat Kebere <kiyyakebere@gmail.com>
2023-10-08 02:54:49 +0300

    :art: Modify Chat UI

--- web/src/app/dashboard/chat/chat.js
+++ web/src/app/dashboard/chat/chat.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Chat() {
   return (
-    <div className="border">
+    <div className="border w-1/2 bg-slate-300 h-5/6 overflow-y-auto">
       <div className="chat chat-start">
         <div className="chat-image avatar">
           <div className="w-12 rounded-full p-3">
@@ -56,6 +56,11 @@ export default function Chat() {
           </div>
         </div>
         <div className="chat-bubble">Make it more lighter</div>
+      </div>
+      <div className="modal-action">
+        <form method="dialog">
+          <button className="btn">Close</button>
+        </form>
       </div>
     </div>
   );
