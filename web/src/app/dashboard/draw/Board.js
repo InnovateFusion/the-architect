54549cc57f234a624a3276c32a3e778b519e56de
Aser Hailu <aserhailu2020@gmail.com>
2023-10-12 00:48:04 +0300

    add draw page

--- web/src/app/dashboard/draw/Board.js
+++ web/src/app/dashboard/draw/Board.js
@@ -1,32 +1,80 @@
 "use client";
 import React, { useRef } from "react";
-import { ReactSketchCanvas } from "react-sketch-canvas";
+import { Excalidraw } from "@excalidraw/excalidraw";
+import { useState, useEffect } from "react";
 
 const styles = {
   border: "0.0625rem solid #9c9c9c",
   borderRadius: "0.25rem",
 };
 
+
+/**
+ * Handles exporting the image.
+ *
+ * @return {void} 
+ */
+
+
 const Canvas = () => {
+  
   const canvasRef = useRef(null);
+  const [Excalidraw, setExcalidraw] = useState(null);
+
+  useEffect(() => {
+    import("@excalidraw/excalidraw").then((comp) => setExcalidraw(comp.Excalidraw));
+    console.log(Excalidraw)
+  }, []);
+
+/**
+ * Handles exporting the image.
+ *
+ * @return {void} 
+ */
 
   const handleExportImage = () => {
-    if (canvasRef.current) {
-      canvasRef.current
-        .exportImage("png")
-        .then((data) => {
-          console.log(data);
-        })
-        .catch((e) => {
-          console.log(e);
-        });
+    if (canvasRef.current && canvasRef.current.toDataURL) {
+      const dataUrl = canvasRef.current.toDataURL();
+      setImageData(dataUrl);
+      sendImageToBackend(dataUrl);
+    } else {
+      console.error("canvasRef is null or doesn't have toDataURL method");
     }
   };
 
+/**
+ * Sends the image data to the backend API for uploading.
+ *
+ * @param {type} imageData - The image data to be uploaded.
+ * @return {type} This function does not return anything.
+ */
+
+  const sendImageToBackend = (imageData) => {
+    fetch("/api/upload-image", {
+      method: "POST",
+      body: JSON.stringify({ image: imageData }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
+
   return (
-    <div className="h-full aspect-square p-4">
-      <ReactSketchCanvas ref={canvasRef} strokeWidth={5} strokeColor="black" />
-      <button onClick={handleExportImage}>Get Image</button>
+    <div className="h-600 aspect-square p-4">
+       {Excalidraw && <Excalidraw ref={canvasRef}
+          width={600}
+          height={'100vh'}
+          initialData={{
+            elements: [],
+            appState: {},
+          }}/>} 
     </div>
   );
 };
--- web/src/app/dashboard/draw/page.js
+++ web/src/app/dashboard/draw/page.js
@@ -5,12 +5,9 @@ import Canvas from "./Board";
 
 const ExampleComponent = () => {
   return (
-    <div className="h-full sm:flex ">
-      <div className="w-full sm:w-1/2 flex items-center justify-center p-4">
+    <div className="h-full ">
+      <div className="w-full  items-center justify-center p-4">
         <Canvas />
-      </div>
-      <div className="w-full sm:w-1/2">
-        <Chat />
       </div>
     </div>
   );
