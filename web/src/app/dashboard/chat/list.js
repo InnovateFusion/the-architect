1866cbf93e797f395988b5e6da60511d6ed78aa2
Aser Hailu <aserhailu2020@gmail.com>
2023-10-15 02:48:38 +0300

    do getImage page

--- web/src/app/dashboard/chat/list.js
+++ web/src/app/dashboard/chat/list.js
@@ -1,7 +1,41 @@
+"use client";
 import Image from "next/image";
-import React from "react";
-
+import React, {useEffect, useState} from "react";
+import Link from "next/link";
 function List() {
+
+  const [data, setData] = useState([]);
+  const userId = 'b14ad8b5-7103-4de0-8e09-6c8b4516f41c'; // replace with the actual user id
+  
+  useEffect(() => {
+
+    const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2OTczMjcyMzEsImVtYWlsIjoiYXNlckBnbWFpbC5jb20iLCJpZCI6ImIxNGFkOGI1LTcxMDMtNGRlMC04ZTA5LTZjOGI0NTE2ZjQxYyIsImZpcnN0X25hbWUiOiJhc2VyIiwibGFzdF9uYW1lIjoiaGFpbHUifQ.HUF7D-gonOy0ONlYfIdIy8L_5a_5UHVVBORlEjAM5M0'
+    const url = `https://the-architect.onrender.com/api/v1/users/b14ad8b5-7103-4de0-8e09-6c8b4516f41c/posts`;
+  
+    fetch(url,
+      {
+        headers: {
+          'Authorization': `Bearer ${token}`
+        }
+      })
+
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        setData(data);
+  
+      })
+      .catch(error => {
+        console.error('There has been a problem with your fetch operation:', error);
+      });
+
+  }, []);
+
+
   return (
     <div>
       <div className="">
@@ -10,7 +44,7 @@ function List() {
             My Designs
           </h2>
           <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-            {[1, 2, 3, 4, 5, 6, 7].map((item, index) => {
+            {data.map((item, index) => {
               return (
                 <div className="group relative" key={index}>
                   <div
@@ -18,13 +52,17 @@ function List() {
                       index % 2 == 0 ? "full" : "1/2"
                     } overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80`}
                   >
-                    <Image
-                      width={200}
-                      height={200}
-                      src="/house.jpg"
-                      alt="Front of men&#039;s Basic Tee in black."
-                      className="h-full w-full object-cover object-center lg:h-full lg:w-full"
-                    />
+                     <Link href={`/chat/${userId}`}>
+                   
+                      <Image
+                          width={200}
+                          height={200}
+                          src="/house.jpg"
+                          alt="Front of men&#039;s Basic Tee in black."
+                          className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                        />    
+                     </Link>
+                   
                   </div>
                 </div>
               );
