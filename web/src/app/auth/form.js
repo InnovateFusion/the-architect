9d90b4b84f71080e71440037ef23c81a2b7a7acf
Bisrat Kebere <kiyyakebere@gmail.com>
2023-10-15 11:26:08 +0300

    :key: Login Authentication

--- web/src/app/auth/form.js
+++ web/src/app/auth/form.js
@@ -1,18 +1,50 @@
 "use client";
 
+import { useRouter } from "next/navigation";
 import * as React from "react";
 
 export function UserAuthForm({ className, ...props }) {
   const [isLoading, setIsLoading] = React.useState(false);
+  const [username, setUsername] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const router = useRouter();
 
   async function onSubmit(event) {
     event.preventDefault();
     setIsLoading(true);
+    const options = {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({ email: username, password: password }),
+    };
+    try {
+      console.log(username);
+      console.log(password);
+      const res = await fetch(
+        "https://the-architect.onrender.com/api/v1/token",
+        options
+      );
+      if (res.status == 200) {
+        const user = await res.json();
 
-    setTimeout(() => {
+        localStorage.setItem("token", user.access_token);
+        router.push("/dashboard/design");
+      }
       setIsLoading(false);
-    }, 3000);
+    } catch {
+      console.log("error");
+      setIsLoading(false);
+    }
   }
+
+  React.useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      router.push("/dashboard/chat");
+    }
+  }, []);
 
   return (
     <div className="grid gap-6" {...props}>
@@ -31,13 +63,40 @@ export function UserAuthForm({ className, ...props }) {
               autoCorrect="off"
               disabled={isLoading}
               className="input input-bordered w-full "
+              onChange={(e) => setUsername(e.target.value)}
+              value={username}
+            />
+
+            <input
+              id="password"
+              placeholder="password"
+              type="password"
+              autoCapitalize="none"
+              autoCorrect="off"
+              disabled={isLoading}
+              className="input input-bordered w-full "
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
             />
           </div>
           <button className="btn btn-primary m-2" disabled={isLoading}>
-            {/* {isLoading && (isLoading
-              <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
-            )} */}
-            Sign In with Email
+            {isLoading && (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99"
+                />
+              </svg>
+            )}
+            Login
           </button>
         </div>
       </form>
@@ -45,20 +104,7 @@ export function UserAuthForm({ className, ...props }) {
         <div className="absolute inset-0 flex items-center">
           <span className="w-full border-t" />
         </div>
-        <div className="relative flex justify-center text-xs uppercase">
-          <span className="bg-background px-2 text-muted-foreground">
-            Or continue with
-          </span>
-        </div>
       </div>
-      <button className="btn btn-outline m-4" disabled={isLoading}>
-        {/* {isLoading ? (
-          <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
-        ) : (
-          <Icons.gitHub className="mr-2 h-4 w-4" />
-        )}{" "} */}
-        Github
-      </button>
     </div>
   );
 }
--- web/src/app/auth/page.js
+++ web/src/app/auth/page.js
@@ -43,26 +43,14 @@ export default function AuthenticationPage() {
             </div>
           </div>
 
-          {/* <div className="relative z-20 mt-auto">
-            <blockquote className="space-y-2">
-              <p className="text-lg">
-                &ldquo;This library has saved me countless hours of work and
-                helped me deliver stunning designs to my clients faster than
-                ever before.&rdquo;
-              </p>
-              <footer className="text-sm">Sofia Davis</footer>
-            </blockquote>
-          </div> */}
+         
         </div>
         <div className="lg:p-8">
           <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
             <div className="flex flex-col space-y-2 text-center">
               <h1 className="text-2xl font-semibold tracking-tight">
-                Create an account
+                Log in to your account
               </h1>
-              <p className="text-sm text-muted-foreground">
-                Enter your email below to create your account
-              </p>
             </div>
             <UserAuthForm />
             <p className="px-8 text-center text-sm text-muted-foreground">
