3df4922b0218e87d453aadc3d668dc02d8e24e38
Bisrat Kebere <kiyyakebere@gmail.com>
2023-10-22 16:35:55 +0300

    :package: Add State Management with Zustand

---
+++ web/src/app/dashboard/design/DesignCard.js
@@ -0,0 +1,58 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+
+const view = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+    className="w-6 h-6"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
+    />
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+    />
+  </svg>
+);
+
+const clone = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+    className="w-6 h-6"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M11.35 3.836c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m8.9-4.414c.376.023.75.05 1.124.08 1.131.094 1.976 1.057 1.976 2.192V16.5A2.25 2.25 0 0118 18.75h-2.25m-7.5-10.5H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V18.75m-7.5-10.5h6.375c.621 0 1.125.504 1.125 1.125v9.375m-8.25-3l1.5 1.5 3-3.75"
+    />
+  </svg>
+);
+
+function DesignCard({ item }) {
+  return (
+    <div className="rounded-lg overflow-hidden mb-8" key={item.id}>
+      <Link href={`/dashboard/design/${item.id}`}>
+        <Image src={item.image} alt={item.id} width={512} height={512} />
+      </Link>
+      <div className="px-6 py-4 flex justify-start gap-3 bg-gray-700">
+        {clone}
+        {view}
+      </div>
+    </div>
+  );
+}
+
+export default DesignCard;
---
+++ web/src/store/designs.js
@@ -0,0 +1,50 @@
+import { create } from "zustand";
+
+const designsStore = create((set, get) => ({
+  designs: [],
+  selectedDesign: null,
+  isLoading: false,
+  error: null,
+
+  // Fetch designs from API
+  fetchDesigns: async () => {
+    set({ isLoading: true });
+    try {
+      const response = await fetch(
+        "https://the-architect.onrender.com/api/v1/posts/all"
+      );
+      const designs = await response.json();
+      set({ designs, isLoading: false });
+    } catch (error) {
+      set({ error, isLoading: false });
+    }
+  },
+
+  // Select a design by ID
+  selectDesign: (id) => {
+    const selectedDesign = get().designs.find((design) => design.id === id);
+    set({ selectedDesign });
+  },
+
+  // Add a new design
+  addDesign: (design) => {
+    const designs = [...get().designs, design];
+    set({ designs });
+  },
+
+  // Update an existing design
+  updateDesign: (id, updatedDesign) => {
+    const designs = get().designs.map((design) =>
+      design.id === id ? updatedDesign : design
+    );
+    set({ designs });
+  },
+
+  // Delete a design by ID
+  deleteDesign: (id) => {
+    const designs = get().designs.filter((design) => design.id !== id);
+    set({ designs });
+  },
+}));
+
+export default designsStore;
