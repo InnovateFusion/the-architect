492841257ff528fc54080dde3df950c3c9f86db1
Bisrat Kebere <kiyyakebere@gmail.com>
2023-10-27 11:09:25 +0300

    :wrench: Config Dark Mode

--- web/next.config.js
+++ web/next.config.js
@@ -21,6 +21,12 @@ const nextConfig = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
 };
