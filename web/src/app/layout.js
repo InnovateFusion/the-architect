41841340b4f98ee42b46ac722d3c321035ea4553
Bisrat Kebere <kiyyakebere@gmail.com>
2023-10-31 09:45:26 +0300

    :rocket: Add Web Analytics

--- web/src/app/layout.js
+++ web/src/app/layout.js
@@ -3,6 +3,8 @@ import { Inter } from "next/font/google";
 import Providers from "./providers";
 import "react-toastify/dist/ReactToastify.css";
 const inter = Inter({ subsets: ["latin"] });
+import { Analytics } from "@vercel/analytics/react";
+import Loglib from "@loglib/tracker/react";
 
 export const metadata = {
   title: "The Architect by Innovate Fusion",
@@ -15,6 +17,12 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <Providers>{children}</Providers>
+        <Analytics />
+        <Loglib
+          config={{
+            id: "architect",
+          }}
+        />
       </body>
     </html>
   );
